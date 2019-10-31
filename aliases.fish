2a66cf586e5f9572e81fa4f78335362d391566d3
Yingqi Jing <350956009@qq.com>
2019-10-31 23:27:30 +0100

    change alias

--- aliases.fish
+++ aliases.fish
@@ -7,10 +7,5 @@ alias ev 'vim ~/.vimrc'
 
 # Quick go
 alias gd 'cd ~/Documents/ && lsf'
-alias gD 'cd ~/Downloads/ && lsf'
-alias gh 'cd ~ && lsf'
-alias gg 'cd ~/Google\ Drive/ && lsf'
-alias gi 'cd ~/git/ && lsf'
-alias gs 'cd ~/switchdrive/ && lsf'
-alias gf 'cd ~/switchdrive/phylogenetic-tools/simmap-eigendecomposition/Main/ && lsf'
+
 
