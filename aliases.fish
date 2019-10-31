0ea5b9f6f655c12b6e39e49f74ae425f488382cf
Yingqi Jing <350956009@qq.com>
2019-10-31 23:36:02 +0100

    update alias

--- aliases.fish
+++ aliases.fish
@@ -1,11 +1,11 @@
 # list files
-alias lsf "clear && ls -v --group-directories-first"
+alias lsf "clear; and ls -v --group-directories-first"
 
 # Quick edits
 alias ef 'vim ~/.config/fish/config.fish'
 alias ev 'vim ~/.vimrc'
 
 # Quick go
-alias gd 'cd ~/Documents/ && lsf'
+# alias gd 'cd ~/Documents/ ; and lsf'
 
 
