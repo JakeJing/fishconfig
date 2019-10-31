b14b671b8193b3ee47713e50654e990d9659d777
Yingqi Jing <350956009@qq.com>
2019-10-31 23:24:04 +0100

    update abbr_fish

--- abbr_fish.sh
+++ abbr_fish.sh
@@ -4,7 +4,6 @@ abbr -a -U -- ex exit
 abbr -a -U -- fc fish_config
 abbr -a -U -- fun functions
 abbr -a -U -- fup fish_update_completions
-abbr -a -U -- img '~/libsixel/converters/img2sixel'
 abbr -a -U -- rg ranger
 abbr -a -U -- vf 'vifm $PWD'
 abbr -a -U -- vi vim
