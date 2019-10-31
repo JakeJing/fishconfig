f592176e8adf13efc4f8171c1729857acc0d8716
Yingqi Jing <350956009@qq.com>
2019-10-31 23:51:45 +0100

    edit abbr fish

---
+++ abbr_fish.fish
@@ -0,0 +1,12 @@
+#! /usr/bin/fish
+
+abbr -a -- ex exit
+abbr -a -- fc fish_config
+abbr -a -- fun functions
+abbr -a -- fup fish_update_completions
+abbr -a -- rg ranger
+abbr -a -- vf 'vifm $PWD'
+abbr -a -- vi vim
+abbr -a -- ff 'ffmpeg -i'
+abbr -a -- chm 'chmod +x *.R'
+
