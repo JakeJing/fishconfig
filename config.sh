7cee9c64cc959842df160f8706135dc9cee4c30b
JakeJing <350956009@qq.com>
2019-11-02 00:12:09 +0100

    add vifmrc

--- config.sh
+++ config.sh
@@ -1,26 +1,31 @@
 #! /usr/bin/bash
 
+#install Linuxbrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
 # install fish
 sudo apt-get install fish
 
 # make a .config folder
 mkdir -p ~/.config/fish
-
-echo "set -g -x fish_greeting ''" > ~/.config/fish/config.fish
+echo "set fish_greeting ''" > ~/.config/fish/config.fish
 
 # install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+brew install fzf
+$(brew --prefix)/opt/fzf/install
 
 # install omf
-curl -L https://get.oh-my.fish > install
-fish install --path=/.local/share/omf --config=/.config/omf
+curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > install
 
 # install vifm
 sudo apt-get install vifm
 
 wget https://raw.githubusercontent.com/vifm/vifm-colors/master/solarized-dark.vifm -P ~/.config/vifm/colors/
 
-sed -i "" 's/colorscheme\ Default/colorscheme\ solorized-dark/g' ~/.config/vifm/vifmrc
+
+sed -i "" 's/colorscheme\ Default/colorscheme\ solarized-dark/g' ~/.config/vifm/vifmrc
 
 
