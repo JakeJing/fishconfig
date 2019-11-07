a36d8f0643bff1b80a4f43ed054b92f0dfdb9a21
JakeJing <350956009@qq.com>
2019-11-07 22:46:29 +0100

    update alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,11 +1,28 @@
 # list files
-alias lsf "clear; and ls -v --group-directories-first"
+alias lsf "clear ; and ls -v --group-directories-first"
+
+# trash-cli
+alias ts "trash"
+alias tsl "trash-list"
+alias tsr "trash-restore"
+alias tsep "trash-empty"
+alias tsrm "trash-rm"
+alias rm "trash"
 
 # Quick edits
 alias ef 'vim ~/.config/fish/config.fish'
 alias ev 'vim ~/.vimrc'
 
 # Quick go
-# alias gd 'cd ~/Documents/ ; and lsf'
+alias gd 'cd ~/Documents/ ; and lsf'
+alias gD 'cd ~/Downloads/ ; and lsf'
+alias gh 'cd ~ ; and lsf'
+alias gg 'cd ~/Google\ Drive/ ; and lsf'
+alias gi 'cd ~/git/ ; and lsf'
+alias gs 'cd ~/switchdrive/ ; and lsf'
+alias gp 'cd ~/switchdrive/phylogenetic-tools/simmap-eigendecomposition/Main/ ; and lsf'
 
+#yank directory and pbpaste
+alias yd 'pwd|pbcopy'
+alias pp 'pbpaste'
 
